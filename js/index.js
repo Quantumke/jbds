f31796daeee99a7d8ae1ed726d63c728aeb619b0
quantumke <nguruben@gmail.com>
2016-08-08 09:57:35 +0300

    pricing efx

--- js/index.js
+++ js/index.js
@@ -1,8 +1,25 @@
 $(function() {
-  $('.button-get-this').hover(function() {
-    $('.select-plan-banner').css('background-color', 'green');
+  $('.button-starter').hover(function() {
+    $('.banner-starter').css('background-color', 'green');
   }, function() {
     // on mouseout, reset the background colour
-    $('.select-plan-banner').css('background-color', '#EFF0F4');
+    $('.banner-starter').css('background-color', '#EFF0F4');
+  });
+});
+
+$(function() {
+  $('.button-best-option').hover(function() {
+    $('.banner-best-option').css('background-color', 'orange');
+  }, function() {
+    // on mouseout, reset the background colour
+    $('.banner-best-option').css('background-color', '#EFF0F4');
+  });
+});
+$(function() {
+  $('.button-last-option').hover(function() {
+    $('.banner-last-option').css('background-color', 'orangered');
+  }, function() {
+    // on mouseout, reset the background colour
+    $('.banner-last-option').css('background-color', '#EFF0F4');
   });
 });
